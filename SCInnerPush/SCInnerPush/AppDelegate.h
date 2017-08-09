ef6e424291001d51157f69ed7be6b37dd60115f4
374186157 <374186157@qq.com>
2017-08-09 17:21:13 +0800

    init project

---
+++ SCInnerPush/SCInnerPush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCInnerPush
+//
+//  Created by xsc on 2017/8/9.
+//  Copyright © 2017年 xsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
